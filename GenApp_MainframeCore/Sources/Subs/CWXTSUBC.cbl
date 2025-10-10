ad45301504e5e6322c081fb7fc311c8bdf867e07
manosing <manosing@AM-manosing-W4.adprod.bmc.com>
2025-10-10 09:47:11 -0500

    committed on 10-10-25

--- GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
@@ -5,7 +5,7 @@
       ******      C O M P U W A R E   C O R P O R A T I O N       ******
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
-      *  XPEDITER/TSO TRAINING SESSIONS.  MKS-01-01-25 16:22           *
+      *  XPEDITER/TSO TRAINING SESSIONS.  MKS-10-10-25 16:22           *
       *                                                                *
       *  CALCULATES COMMISIONS FOR SALES AND MANAGEMENT EMPLOYEE       *
       *  BASED ON MONTHLY SALES RATES                                  *

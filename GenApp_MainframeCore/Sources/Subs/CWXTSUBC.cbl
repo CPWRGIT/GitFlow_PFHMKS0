b493bda4b813c6b9be1bf7b66b90d4345865e604
Manoj Singh <manoj_singh@bmc.com>
2025-11-07 12:38:15 -0600

    11-07-25-001

--- GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
+++ GenApp_MainframeCore/Sources/Subs/CWXTSUBC.cbl
@@ -5,7 +5,7 @@
       ******      C O M P U W A R E   C O R P O R A T I O N       ******
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
-      *  XPEDITER/TSO TRAINING SESSIONS.  MKS-10-16-25 16:22           *
+      *  XPEDITER/TSO TRAINING SESSIONS.  MKS-11-07-25 16:21           *
       *                                                                *
       *  CALCULATES COMMISIONS FOR SALES AND MANAGEMENT EMPLOYEE       *
       *  BASED ON MONTHLY SALES RATES                                  *
